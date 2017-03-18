86fb18d0c4dcac45d098de262d7f381925ab43ee
flyover <flyover.gu@gmail.com>
2017-03-18 11:56:23 +0800

    f

--- labs/weibo/index.js
+++ labs/weibo/index.js
@@ -1,40 +1,59 @@
-
-function getOneUrl($video, done) {
-    function foo($video, done) {
-        var src = $video.find('video').attr('src');
-        if (src) {
-            setTimeout(function() {
+;(function() {
+    function getOneUrl($video, done) {
+        function foo($video, done) {
+            var src = $video.find('video').attr('src');
+            if (src) {
                 $video.find('.hv-ring').click();
-            }, 1000);
-            done(src, $video);
-        } else {
-            setTimeout(function() {
-                foo($video, done);
-            }, 100);
+                done(src, $video);
+            } else {
+                setTimeout(function() {
+                    foo($video, done);
+                }, 100);
+            }
+        }
+        $video.find('.hv-ring').click();
+        foo($video, done);
+    }
+
+    function getDom(url) {
+        var dom = $('<a href="' + url + '" download style=" position: absolute; z-index: 99999; color: #fff; background: rgba(0, 0, 0, 0.60); font-size: 16px; bottom: 0px; right: 0px; padding: 3px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ">下载视频</a>');
+        if (navigator.userAgent.indexOf("MSIE") != -1) {
+            dom.html('下载地址(右键另存为)');
+        }
+        return dom;
+    }
+
+    function getUrl() {
+        var $videoList = $('.html5-video');
+        for (var i = 0; i < $videoList.length; i++) {
+            getOneUrl($videoList.eq(i), function(url, $video) {
+                if (!/\/tv\//.test(document.location.pathname)) {
+                    $video.parent().css({position: 'relative'});
+                }
+                $video.after(getDom(url));
+            });
         }
     }
-    $video.find('.hv-ring').click();
-    foo($video, done);
-}
 
-function getUrl() {
-    var $videoList = $('.html5-video');
-    console.log($videoList);
-    for (var i = 0; i < $videoList.length; i++) {
-        getOneUrl($videoList.eq(i), function(url, $video) {
-            console.log(url);
-            $video.parent().append('<a href="' + url + '" download="1.mp4">下载</a>')
-        });
+    function isSupportDomain() {
+        if (!/weibo\.com/.test(document.location.host)) {
+            document.location.href="http://indexof.site/labs/weibo/index.html";
+            return false;
+        }
+        return true;
     }
-}
 
+    function start() {
+        if (isSupportDomain()) {
+            var node = document.createElement('script');
+            node.src = '//cdn.bootcss.com/zepto/1.2.0/zepto.min.js';
+            node.onload = function() {
+                getUrl();
+            }
+            document.body.appendChild(node);
+        }
+    }
 
-var node = document.createElement('script');
-node.src = 'http://zeptojs.com/zepto.min.js';
-node.onload = function() {
-    getUrl();
-}
-document.body.appendChild(node);
+    start();
 
-
-
+})();
